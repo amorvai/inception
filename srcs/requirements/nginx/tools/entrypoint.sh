341438a6093a759e5fcfaf361b5abb593060a5d1
Anna <amorvai@stud.hs-heilbronn.de>
2025-06-17 20:09:23 +0200

    run nginx container with entrypoint script

--- srcs/requirements/nginx/tools/entrypoint.sh
+++ srcs/requirements/nginx/tools/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-# start nginx in foreground (to keep container running)
-# [-g] set global configuration directives
-nginx -g 'daemon off;'
+### start nginx in foreground (to keep container running)
+### [-g] set global configuration directives
+# nginx -g 'daemon off;'
+
+if [ "$1" = 'sh' ]; then
+  exec "$@"
+else
+  nginx -g 'daemon off;'
+fi
