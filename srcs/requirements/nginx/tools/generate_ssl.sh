fd3fb4073cfe6275c5d0057eee4a713840292484
Anna <amorvai@stud.hs-heilbronn.de>
2025-06-18 11:31:15 +0200

    set permissions for key and cert expl.

--- srcs/requirements/nginx/tools/generate_ssl.sh
+++ srcs/requirements/nginx/tools/generate_ssl.sh
@@ -7,12 +7,19 @@ CERT="$SSL_DIR/nginx.crt"
 # set -x
 
 if [ ! -f "$CERT" ] || [ ! -f "$KEY" ]; then
-  echo "Generating self-signed SSL certificate..."
   mkdir -p "$SSL_DIR"
+  # Create the directory if it does not exist
+
+  echo "Generating self-signed SSL certificate ..."
   openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
     -keyout "$KEY" \
     -out "$CERT" \
     -subj "/C=DE/ST=BW/L=Heilbronn/O=42/OU=42 Heilbronn/CN=amorvai.42.fr/UID=amorvai"
+  chmod 600 "$KEY"
+  chmod 644 "$CERT"
+  # Set permissions for the key and certificate
+
+  echo "SSL certificate generated successfully."
 else
-  echo "SSL certificate already exists."
+    echo "SSL certificate already exists."
 fi
