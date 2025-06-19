6efde7b3237fab0923ca4d124d780867d2b08d9b
Anna <amorvai@stud.hs-heilbronn.de>
2025-06-19 13:49:42 +0200

    commenting out crappy catch

--- srcs/requirements/nginx/tools/entrypoint.sh
+++ srcs/requirements/nginx/tools/entrypoint.sh
@@ -2,15 +2,15 @@
 
 sh /usr/local/bin/generate_ssl.sh
 
-### start nginx in foreground (to keep container running)
-### [-g] set global configuration directives
-# nginx -g 'daemon off;'
+# start nginx in foreground (to keep container running)
+# [-g] set global configuration directives
+nginx -g 'daemon off;'
 
-if [ "$1" = 'sh' ]; then
-  exec "$1"
-  # Run the interactive shell if 'sh' is the first argument,
-  #  keeping the container in the foreground.
-#   exec "$@"
-else
-  nginx -g 'daemon off;'
-fi
+# if [ "$1" = 'sh' ]; then                      # very very unpretty
+#   exec "$1"
+#   # Run the interactive shell if 'sh' is the first argument,
+#   #  keeping the container in the foreground.
+# #   exec "$@"
+# else
+#   nginx -g 'daemon off;'
+# fi
